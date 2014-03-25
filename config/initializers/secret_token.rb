da7b701249c2f2bb2b7d339734973462646e9cab
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-03-24 20:49:47 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kopool::Application.config.secret_key_base = 'eecd4477882ca3c7f1a523bf61c4b8cd044bc43a69c8cbbd4d64102b6174b0c44a3fa53c9c0c737181ce9531572cc54174866fe43cd82e26902c48321a8ee29b'

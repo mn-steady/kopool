c7e7e6aea5369ae6cca1e62beb9f2c7657f37613
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-04-14 19:58:05 -0500

    Initial week validations

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,2 +1,9 @@
 class Week < ActiveRecord::Base
+	has_many :matchups
+	belongs_to :season
+
+	validates_presence_of :week_number
+	validates_presence_of :start_date
+	validates_presence_of :end_date
+	validates_presence_of :deadline
 end

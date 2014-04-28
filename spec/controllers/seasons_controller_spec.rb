aa9b5fb7ca796fd228765aed684c734efb63edd0
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-04-28 18:48:10 -0500

    First seasons spec

--- spec/controllers/seasons_controller_spec.rb
+++ spec/controllers/seasons_controller_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe SeasonsController do
 	pending "Admins can change the open/closed status of a season"
 	pending "Non-admins cannot change the open/closed status of a season"
+
+	describe "GET new" do
+		it "sets the @season variable"
+	end
 end

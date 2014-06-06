b0d0ed55d4f461353eb27f240f1b06726b684097
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-06-05 21:25:34 -0500

    Trying to beef up matchup UI

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,18 +1,14 @@
 class MatchupsController < ApplicationController
-	def index
-		if is_admin_user
-      Rails.logger.debug("(MatchupsController.index) is admin")
-      @week = Week.find(params[:week_id])
-      @matchups = Matchup.where(week: @week)
+	before_filter :verify_admin_user, only: [:show, :update]
+  before_filter :verify_any_user, only: [:index]
 
-      respond_to do | format |
-        format.json {render :json => @matchups.to_json(include: [{ home_team: { only: [:name, :id] }}, away_team: {only: [:name, :id]}] ) }
-      end
-    else
-      Rails.logger.error("(MatchupsController.index) unauthorized")
-      respond_to do | format |
-        format.json { render :json => [], :status => :unauthorized }
-      end
+  def index
+
+    @week = Week.find(params[:week_id])
+    @matchups = Matchup.where(week: @week)
+
+    respond_to do | format |
+      format.json {render :json => @matchups.to_json(include: [{ home_team: { only: [:name, :id] }}, away_team: {only: [:name, :id]}] ) }
     end
 	end
 

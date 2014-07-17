245664f20dca9768c45f7fc5129e938849ada661
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-07-16 21:09:46 -0500

    Modal forms and WebState Week work

--- app/models/week.rb
+++ app/models/week.rb
@@ -34,16 +34,14 @@ class Week < ActiveRecord::Base
 		webstate = WebState.first
 		if self.id == webstate.week_id
 			self.close_week_for_picks!
-			self.update_attributes(current_week: false)
 			# TODO: Decide what we want to do to end a season
 			if self.week_number < WEEKS_IN_SEASON # Don't error out if this is the last week in the season
 				next_week_number = self.week_number + 1
 				next_week = Week.where(season: season).where(week_number: next_week_number).first
-				next_week.update_attributes(current_week: true)
 				webstate.update_attributes(week_id: next_week.id)
 				return true
 			else
-				Rails.logger.debug("We have reached the end of the season")
+				Rails.logger.error("We have reached the end of the season")
 				# TODO: Automatically close the season?
 				return true
 			end

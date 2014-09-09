a0bdb9add4ac889bdb51728476e4581b17c40c7b
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-09-08 22:37:59 -0500

    URL issue in email fixed

---
+++ app/mailers/password_mailer.rb
@@ -0,0 +1,8 @@
+class PasswordMailer < Devise::Mailer   
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+
+  def reset_password_instructions(record, token, opts={})
+    super
+  end
+end

def04782ce7802121064ea7286d6ff84238fb226
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-09-23 19:48:15 -0500

    Set capybara host and port explicitly to play nicely with selenium and rails_api_resource

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'capybara/rspec'
+
+Capybara.app_host = "http://localhost:3000"
+Capybara.server_host = "localhost"
+Capybara.server_port = "3000"
 
 Capybara.default_driver = :selenium
 

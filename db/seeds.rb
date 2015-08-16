229e5036b57d14500ba22c19d9c8de9aad07bed1
Jack Collins <BlueSkyFlyer2@gmail.com>
2015-08-16 09:20:33 -0600

    Scope pool entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,36 @@
 			user4 = User.create!(email: "user4@example.com", password: "password", password_confirmation: "password", phone: "4234567890", name: "User 4")
 			admin = User.create!(email: "admin@example.com", password: "password", password_confirmation: "password", phone: "1234567890", name: "Administrator", admin: true)
 
+			last_season = Season.create!(year: 2016, name: "2014 Season", entry_fee: 50)
+			previous_week1 = Week.create!(season: last_season, week_number: 1, start_date: DateTime.new(2014, 8, 5), deadline: DateTime.new(2014, 8, 8), end_date: DateTime.new(2014, 8, 11))
+			previous_week2 = Week.create!(season: last_season, week_number: 2, start_date: DateTime.new(2014, 8, 12), deadline: DateTime.new(2014, 8, 15), end_date: DateTime.new(2014, 8, 18))
+			previous_week3 = Week.create!(season: last_season, week_number: 3, start_date: DateTime.new(2014, 8, 19), deadline: DateTime.new(2014, 8, 22), end_date: DateTime.new(2014, 8, 25))
+			previous_week4 = Week.create!(season: last_season, week_number: 4, start_date: DateTime.new(2014, 8, 26), deadline: DateTime.new(2014, 8, 29), end_date: DateTime.new(2014, 9, 1))
+
 			season = Season.create!(year: 2015, name: "2015 Season", entry_fee: 50)
-			season2 = Season.create!(year: 2016, name: "2016 Season", entry_fee: 50)
 			week1 = Week.create!(season: season, week_number: 1, start_date: DateTime.new(2015, 8, 5), deadline: DateTime.new(2015, 8, 8), end_date: DateTime.new(2015, 8, 11))
 			week2 = Week.create!(season: season, week_number: 2, start_date: DateTime.new(2015, 8, 12), deadline: DateTime.new(2015, 8, 15), end_date: DateTime.new(2015, 8, 18))
 			week3 = Week.create!(season: season, week_number: 3, start_date: DateTime.new(2015, 8, 19), deadline: DateTime.new(2015, 8, 22), end_date: DateTime.new(2015, 8, 25))
 			week4 = Week.create!(season: season, week_number: 4, start_date: DateTime.new(2015, 8, 26), deadline: DateTime.new(2015, 8, 29), end_date: DateTime.new(2015, 9, 1))
 			
 			web_state = WebState.create(week_id: week4.id, season_id: season.id)
+
+			# Last season's pool entries
+
+			pool_entry1 = PoolEntry.create!(user: user1, team_name: "Last Season User 1 Team One", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week1)
+			pool_entry2 = PoolEntry.create!(user: user1, team_name: "Last Season User 1 Team Two", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week2)
+			pool_entry3 = PoolEntry.create!(user: user1, team_name: "Last Season User 1 Team Three", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week3)
+			pool_entry4 = PoolEntry.create!(user: user2, team_name: "Last Season User 2 Team One", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week1)
+			pool_entry5 = PoolEntry.create!(user: user2, team_name: "Last Season User 2 Team Two", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week2)
+			pool_entry6 = PoolEntry.create!(user: user2, team_name: "Last Season User 2 Team Three", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week3)
+			pool_entry7 = PoolEntry.create!(user: user3, team_name: "Last Season User 3 Team One", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week1)
+			pool_entry8 = PoolEntry.create!(user: user3, team_name: "Last Season User 3 Team Two", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week2)
+			pool_entry9 = PoolEntry.create!(user: user3, team_name: "Last Season User 3 Team Three", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week3)
+			pool_entry10 = PoolEntry.create!(user: user4, team_name: "Last Season User 4 Team One", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week1)
+			pool_entry11 = PoolEntry.create!(user: user4, team_name: "Last Season User 4 Team Two", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week2)
+			pool_entry12 = PoolEntry.create!(user: user4, team_name: "Last Season User 4 Team Three", paid: true, season_id: last_season.id, knocked_out: true, knocked_out_week_id: previous_week3)
+
+			# New Pool Entry
 
 			pool_entry1 = PoolEntry.create!(user: user1, team_name: "User 1 Team One", paid: true, season_id: season.id)
 			pool_entry2 = PoolEntry.create!(user: user1, team_name: "User 1 Team Two", paid: true, season_id: season.id)
@@ -86,60 +108,60 @@
 			Matchup.handle_matchup_outcome!(matchup1_2.id)
 			Matchup.handle_matchup_outcome!(matchup1_3.id)
 
-			# Week 2 Picks
+			# # Week 2 Picks
 
-			pick2_2 = Pick.create(pool_entry_id: pool_entry2.id, week_id: week2.id, team_id: packers.id, matchup_id: matchup2_2.id)
-			pick2_3 = Pick.create(pool_entry_id: pool_entry3.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
-			pick2_6 = Pick.create(pool_entry_id: pool_entry6.id, week_id: week2.id, team_id: chargers.id, matchup_id: matchup2_3.id)
-			pick2_7 = Pick.create(pool_entry_id: pool_entry7.id, week_id: week2.id, team_id: broncos.id, matchup_id: matchup2_1.id)
-			pick2_8 = Pick.create(pool_entry_id: pool_entry8.id, week_id: week2.id, team_id: lions.id, matchup_id: matchup2_2.id)
-			pick2_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
-			pick2_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week2.id, team_id: vikings.id, matchup_id: matchup2_1.id)
-			pick2_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week2.id, team_id: lions.id, matchup_id: matchup2_2.id)
-			pick2_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
+			# pick2_2 = Pick.create(pool_entry_id: pool_entry2.id, week_id: week2.id, team_id: packers.id, matchup_id: matchup2_2.id)
+			# pick2_3 = Pick.create(pool_entry_id: pool_entry3.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
+			# pick2_6 = Pick.create(pool_entry_id: pool_entry6.id, week_id: week2.id, team_id: chargers.id, matchup_id: matchup2_3.id)
+			# pick2_7 = Pick.create(pool_entry_id: pool_entry7.id, week_id: week2.id, team_id: broncos.id, matchup_id: matchup2_1.id)
+			# pick2_8 = Pick.create(pool_entry_id: pool_entry8.id, week_id: week2.id, team_id: lions.id, matchup_id: matchup2_2.id)
+			# pick2_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
+			# pick2_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week2.id, team_id: vikings.id, matchup_id: matchup2_1.id)
+			# pick2_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week2.id, team_id: lions.id, matchup_id: matchup2_2.id)
+			# pick2_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week2.id, team_id: steelers.id, matchup_id: matchup2_3.id)
 
-			# Handle Week 2 Outcomes
+			# # Handle Week 2 Outcomes
 
-			matchup2_1.update_attributes(winning_team_id: vikings.id)
-			matchup2_2.update_attributes(winning_team_id: lions.id)
-			matchup2_3.update_attributes(winning_team_id: steelers.id)
-			Matchup.handle_matchup_outcome!(matchup2_1.id)
-			Matchup.handle_matchup_outcome!(matchup2_2.id)
-			Matchup.handle_matchup_outcome!(matchup2_3.id)
+			# matchup2_1.update_attributes(winning_team_id: vikings.id)
+			# matchup2_2.update_attributes(winning_team_id: lions.id)
+			# matchup2_3.update_attributes(winning_team_id: steelers.id)
+			# Matchup.handle_matchup_outcome!(matchup2_1.id)
+			# Matchup.handle_matchup_outcome!(matchup2_2.id)
+			# Matchup.handle_matchup_outcome!(matchup2_3.id)
 
-			# Week 3 Picks
+			# # Week 3 Picks
 
-			pick3_3 = Pick.create(pool_entry_id: pool_entry3.id, week_id: week3.id, team_id: steelers.id, matchup_id: matchup3_3.id)
-			pick3_8 = Pick.create(pool_entry_id: pool_entry8.id, week_id: week3.id, team_id: lions.id, matchup_id: matchup3_2.id)
-			pick3_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week3.id, team_id: chargers.id, matchup_id: matchup3_3.id)
-			pick3_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week3.id, team_id: broncos.id, matchup_id: matchup3_1.id)
-			pick3_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week3.id, team_id: packers.id, matchup_id: matchup3_2.id)
-			pick3_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week3.id, team_id: chargers.id, matchup_id: matchup3_3.id)	
+			# pick3_3 = Pick.create(pool_entry_id: pool_entry3.id, week_id: week3.id, team_id: steelers.id, matchup_id: matchup3_3.id)
+			# pick3_8 = Pick.create(pool_entry_id: pool_entry8.id, week_id: week3.id, team_id: lions.id, matchup_id: matchup3_2.id)
+			# pick3_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week3.id, team_id: chargers.id, matchup_id: matchup3_3.id)
+			# pick3_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week3.id, team_id: broncos.id, matchup_id: matchup3_1.id)
+			# pick3_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week3.id, team_id: packers.id, matchup_id: matchup3_2.id)
+			# pick3_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week3.id, team_id: chargers.id, matchup_id: matchup3_3.id)	
 
-			# Handle Week 3 Outcomes
+			# # Handle Week 3 Outcomes
 
-			matchup3_1.update_attributes(winning_team_id: broncos.id)
-			matchup3_2.update_attributes(winning_team_id: packers.id)
-			matchup3_3.update_attributes(winning_team_id: chargers.id)
-			Matchup.handle_matchup_outcome!(matchup3_1.id)
-			Matchup.handle_matchup_outcome!(matchup3_2.id)
-			Matchup.handle_matchup_outcome!(matchup3_3.id)
+			# matchup3_1.update_attributes(winning_team_id: broncos.id)
+			# matchup3_2.update_attributes(winning_team_id: packers.id)
+			# matchup3_3.update_attributes(winning_team_id: chargers.id)
+			# Matchup.handle_matchup_outcome!(matchup3_1.id)
+			# Matchup.handle_matchup_outcome!(matchup3_2.id)
+			# Matchup.handle_matchup_outcome!(matchup3_3.id)
 
-			# Week 4 Picks
+			# # Week 4 Picks
 
-			pick4_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week4.id, team_id: steelers.id, matchup_id: matchup4_3.id)
-			pick4_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week4.id, team_id: broncos.id, matchup_id: matchup4_1.id)
-			pick4_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week4.id, team_id: lions.id, matchup_id: matchup4_2.id)
-			pick4_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week4.id, team_id: steelers.id, matchup_id: matchup4_3.id)	
+			# pick4_9 = Pick.create(pool_entry_id: pool_entry9.id, week_id: week4.id, team_id: steelers.id, matchup_id: matchup4_3.id)
+			# pick4_10 = Pick.create(pool_entry_id: pool_entry10.id, week_id: week4.id, team_id: broncos.id, matchup_id: matchup4_1.id)
+			# pick4_11 = Pick.create(pool_entry_id: pool_entry11.id, week_id: week4.id, team_id: lions.id, matchup_id: matchup4_2.id)
+			# pick4_12 = Pick.create(pool_entry_id: pool_entry12.id, week_id: week4.id, team_id: steelers.id, matchup_id: matchup4_3.id)	
 
-			# Handle Week 4 Outcomes
+			# # Handle Week 4 Outcomes
 
-			matchup4_1.update_attributes(winning_team_id: vikings.id)
-			matchup4_2.update_attributes(winning_team_id: lions.id)
-			matchup4_3.update_attributes(winning_team_id: steelers.id)
-			Matchup.handle_matchup_outcome!(matchup4_1.id)
-			Matchup.handle_matchup_outcome!(matchup4_2.id)
-			Matchup.handle_matchup_outcome!(matchup4_3.id)
+			# matchup4_1.update_attributes(winning_team_id: vikings.id)
+			# matchup4_2.update_attributes(winning_team_id: lions.id)
+			# matchup4_3.update_attributes(winning_team_id: steelers.id)
+			# Matchup.handle_matchup_outcome!(matchup4_1.id)
+			# Matchup.handle_matchup_outcome!(matchup4_2.id)
+			# Matchup.handle_matchup_outcome!(matchup4_3.id)
 
 # create a base admin user so we can login
 # For some reason I was only able to do this from the Rails Console...

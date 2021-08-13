d13ea128c1f55d9a74a97e2dca1091fe02293561
Andrew Steadman <ajsteadman89@gmail.com>
2021-08-13 18:10:26 -0500

    updated sendgrid api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Kopool::Application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME_New'],
-    :password       => ENV['SENDGRID_PASSWORD_New'],
+    :user_name      => ENV['apikey'],
+    :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }

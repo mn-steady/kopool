26a362d19af3cb34d82356eaaab7d7c2a63ea1be
Andrew Steadman <ajsteadman89@gmail.com>
2021-08-13 17:54:47 -0500

    updated sendgrid config vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Kopool::Application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME_New'],
+    :password       => ENV['SENDGRID_PASSWORD_New'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }

5a09164f97fe1b15535bcdb4f9b59be55195cfac
Ghulam Jilani <ghulamjilanideveloper@gmail.com>
2023-04-07 11:35:47 +0500

    app update to rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -22,19 +22,15 @@ Bundler.require(*Rails.groups)
 module Kopool
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
     config.time_zone = 'Eastern Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.to_prepare do
        DeviseController.respond_to :html, :json

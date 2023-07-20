7a80f6eacad4a6130522763f02416766f141b79d
Andrew Steadman <ajsteadman89@gmail.com>
2023-07-20 17:30:08 -0500

    updated main page bubble dimensions

--- app/models/main_page_bubble.rb
+++ app/models/main_page_bubble.rb
@@ -5,7 +5,7 @@ class MainPageBubble < ApplicationRecord
 
   def image_url
     begin
-      image.variant(resize_to_fill: [360, 640]).processed.url
+      image.variant(resize_to_fill: [640, 360]).processed.url
     rescue => e
       Rails.logger.error "Error generating thumbnail URL: #{e.message}"
       MainPageBubble.default_image_url

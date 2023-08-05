e22488c3e9fbcfd6592f2fd46626b086cf5b931e
Andrew Steadman <ajsteadman89@gmail.com>
2023-08-04 19:13:00 -0500

    change pic height

--- app/models/main_page_bubble.rb
+++ app/models/main_page_bubble.rb
@@ -5,7 +5,7 @@ class MainPageBubble < ApplicationRecord
 
   def image_url
     begin
-      image.variant(resize_to_fill: [858, 457] ).processed.url
+      image.variant(resize_to_fill: [715, 381] ).processed.url
     rescue => e
       Rails.logger.error "Error generating thumbnail URL: #{e.message}"
       MainPageBubble.default_image_url
